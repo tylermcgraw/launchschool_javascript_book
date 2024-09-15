1850bfa35c414e3806daf8e3ae29bb5ab2c4f3c0
Tyler McGraw <tyler.a.mcgraw@gmail.com>
2024-09-14 22:26:32 -0400

    functions exercises

---
+++ functions/exercises.js
@@ -0,0 +1,61 @@
+// Exercise 1
+// 1, because the second bar variable is only in the scope of the function
+
+// Exercise 4
+// nothing is logged to console
+
+// Exercise 5
+// once again nothing is logged to console
+
+// Exercise 6
+/*
+the function arguments
+  2, 3, 4
+the function body
+  let result = num1 * num2 * num3;
+  return result;
+the function declaration
+  function multiplyNumbers(num1, num2, num3) {
+    let result = num1 * num2 * num3;
+    return result;
+  }
+the function invocation
+  multiplyNumbers(2, 3, 4)
+the function name
+  multiplyNumbers
+the function parameters
+  num1, num2, num3
+the function return value
+  result
+the names of all variables in this program
+  num1, num2, num3, result, product
+*/
+
+// Exercise 7
+// Hello
+// undefined
+
+// Exercise 8
+// 42
+// 3.1415
+
+// Exercise 9
+/*
+multiply left right
+product left right
+product
+getNumber prompt
+parsefloat question prompt
+left getNumber
+right getNumber
+console left right multiply
+*/
+
+// Exercise 10
+/*
+Global: multiply, getNumber, parseFloat, question, left, right
+Local: product, prompt, left, right
+*/
+
+// Exercise 11
+// no. The top left and right variables are parameters for a function and so are local, whereas the bottom ones are global variables
